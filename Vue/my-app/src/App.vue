fa5ca932f396b697b3110c3c8ae48b58c3d79f98
MA806P <mayp126@126.com>
2019-06-22 17:25:01 +0800

    add vue files

---
+++ Vue/my-app/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    {{msg}}
+    <div>
+      <input type="text" v-model="info">
+      <button v-on:click="handleClick">添加</button>
+    </div>
+    <ul>
+      <!-- <li v-for='item in list'>{{item}}</li> -->
+      <todo-item v-for="item in list" :key="item">
+        <template v-slot:item='itemProps'>
+          <span :style="{fontSize: '20px', color: itemProps.checked ? 'red' : 'blue'}">{{item}}</span>
+        </template>
+      </todo-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import TodoItem from "./components/TodoItem.vue";
+
+export default {
+  name: "app",
+  data() {
+    return {
+      msg: "hello vue",
+      info: "",
+      list: []
+    };
+  },
+
+  methods: {
+    handleClick() {
+      this.list.push(this.info);
+      this.info = "";
+    }
+  },
+  components: {
+    TodoItem
+  }
+};
+</script>
+
+<style>
+/* #app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
